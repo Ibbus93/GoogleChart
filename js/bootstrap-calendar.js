634babd808748d67f892db6f811a094a8bc6b05b
OPTC-Agenda <ibbus93@gmail.com>
2017-06-29 12:22:11 +0200

    Various test on bootstrap calendar

--- js/bootstrap-calendar.js
+++ js/bootstrap-calendar.js
@@ -3,5 +3,23 @@
  */
 
 $(function(){
-  $('#calendar').calendar();
+
+  var currentYear = new Date().getFullYear();
+  var orangeDate = new Date(currentYear, 2, 17).getTime();
+  var dateStart = new Date(currentYear, 2, 10);
+  var dateEnd = new Date(currentYear, 2, 13);
+
+  $('#calendar').calendar({
+      customDayRenderer: function (element, date) {
+          if(date.getTime() >= dateStart && date.getTime() <= dateEnd || date.getTime() == orangeDate) {
+            $(element).css('background-color', '#ed6900');
+            $(element).css('color', 'white');
+            $(element).css('border-radius', '15px');
+          }
+      }
+  });
+
+  $('#calendar').data('calendar').setMinDate(new Date(2017, 2, 8));
+  $('#calendar').data('calendar').setMaxDate(new Date(2017, 2, 20));
+
 });
